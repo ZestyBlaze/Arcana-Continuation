b2f968570ae2425e6f57a4662bf7316c88105e91
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-10-27 23:04:33 +0000

    Initial Push of Base

---
+++ src/main/java/dev/zestyblaze/arcana/Arcana.java
@@ -0,0 +1,12 @@
+package dev.zestyblaze.arcana;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Arcana.MODID)
+public class Arcana {
+    public static final String MODID = "arcana";
+
+    public Arcana(IEventBus modEventBus, ModContainer modContainer) {}
+}
